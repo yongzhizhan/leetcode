aed65502184ed4e1f6a0017ec5329e4aef74ce05
湛永志 <yongzhi.zhan@gmail.com>
2016-10-28 07:10:22 +0800

    翻转整数的实现

---
+++ src/main/java/cn/zhanyongzhi/leetcode/ReverseInteger.java
@@ -0,0 +1,44 @@
+package cn.zhanyongzhi.leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int multiRet = 1;
+
+        if(x < 0){
+            x = x * -1;
+            multiRet = -1;
+        }
+
+        //x * -1 overflow case
+        if(x < 0)
+            return 0;
+
+        if(x < 10)
+            return x * multiRet;
+
+        int val;
+
+        //skip zero
+        for(;;){
+            val = x % 10;
+            x = x / 10;
+
+            if(0 != val)
+                break;
+        }
+
+        //long contain int value, invoid overflow case happen
+        long result = 0;
+        while(0 != x || 0 != val){
+            result = result * 10 + val;
+
+            val = x % 10;
+            x = x / 10;
+        }
+
+        if(result > Integer.MAX_VALUE)
+            return 0;
+
+        return (int)result * multiRet;
+    }
+}
